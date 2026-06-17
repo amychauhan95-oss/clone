d22c639a2b3fac9b25a703e23e20b243e97164f7
amychauhan95-oss <amychauhan95@gmail.com>
2026-06-18 00:39:45 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 module.exports = async (req, res) => {
-  const shopifyDomain = "frontendnode-production.up.railway.app";
+  const shopifyDomain = "jobsnearme.jobjaniye.com";
   const proxyHost = req.headers.host;
 
   const targetURL = `https://${shopifyDomain}${req.url}`;
